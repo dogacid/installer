9ef6a964643b727ca941070e041d4b38c78e87cf
Marc Galbraith <marc.s.galbraith@gmail.com>
2025-10-25 00:07:09 +0000

    Fix up pass install for secure credential store on dev machines

--- ubuntu/common/install_pass.sh
+++ ubuntu/common/install_pass.sh
@@ -9,7 +9,14 @@ echo "------------------------------------------------"
 if command_exists pass; then
   echo "pass is already installed."
 else
-  sudo apt update && sudo apt install pass
+  sudo apt update && sudo apt install pass gnupg2
+  gpg --full-generate-key
+  KEY_ID=$(gpg --list-secret-keys --keyid-format=long | grep '^sec' | tail -1 | awk '{print $2}' | cut -d'/' -f2)
+  pass init $KEY_ID
+  wget https://github.com/docker/docker-credential-helpers/releases/download/v0.6.4/docker-credential-pass-v0.6.4-amd64.tar.gz
+  tar xvzf docker-credential-pass-v0.6.4-amd64.tar.gz
+  sudo mv docker-credential-pass /usr/local/bin/
+  chmod +x /usr/local/bin/docker-credential-pass
 fi
 
 echo "--- pass setup complete. ---"
